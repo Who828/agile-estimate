818c848c5685dd04b6842eb065fb35873e8dc0e3
Srihari Sriraman <srihari@c42.in>
2013-03-21 23:21:19 +0530

    Refactored some javascript

--- resources/public/interface.js
+++ resources/public/interface.js
@@ -2,33 +2,34 @@ function abracadabra () {
   $(".dont-show")[0].click();
 }
 
-$(function() {
-  $(".velocity.slider").slider({
+function initialize (velocity, stories) {
+  $("input#velocity").val(velocity);
+  $("input#stories").val(stories);
+  abracadabra();
+}
+
+function setUpSliders () {
+  var opts = {
     min: 0,
     max: 50,
     value: 10,
     animate: true,
-    orientation: "vertical",
-     slide: function( event, ui ) {
-      $("input#velocity").val(ui.value);
-      abracadabra();
-    }
+    orientation: "vertical"
+  };
+
+  vSlider = $(".velocity.slider").slider(opts);
+  sSlider = $(".stories.slider").slider(opts);
+  vSlider.bind('slide', function( event, ui ) {
+    $("input#velocity").val(ui.value);
+    abracadabra();
   });
-
-  $(".stories.slider").slider({
-    min: 0,
-    max: 50,
-    value: 7,
-    animate: true,
-    orientation: "vertical",
-     slide: function( event, ui ) {
-      $("input#stories").val(ui.value);
-      abracadabra();
-    }
+  sSlider.bind('slide', function( event, ui ) {
+    $("input#stories").val(ui.value);
+    abracadabra();
   });
+}
 
-
-  $("input#velocity").val(10);
-  $("input#stories").val(7);
-  abracadabra();
+$(function() {
+  initialize(10, 6);
+  setUpSliders();
 });
