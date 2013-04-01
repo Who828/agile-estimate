484cf6fd8f4942ae0ec9646b1d019c9779f9688d
Srihari Sriraman <srihari@c42.in>
2013-04-01 18:38:58 +0530

    Added a bg image and generated js array

--- src/cljs/estimate.cljs
+++ src/cljs/estimate.cljs
@@ -18,8 +18,21 @@
   (shuffle (rand-nth coll
    )))))
 
+(defn clj->js
+  "Recursively transforms ClojureScript maps into Javascript objects,
+   other ClojureScript colls into JavaScript arrays, and ClojureScript
+   keywords into JavaScript strings."
+  [x]
+  (cond
+    (string? x) x
+    (keyword? x) (name x)
+    (map? x) (.strobj (reduce (fn [m [k v]]
+               (assoc m (clj->js k) (clj->js v))) {} x))
+    (coll? x) (apply array (map clj->js x))
+    :else x))
+
 (event/listen
   stats-button
   "click"
-  (fn [] (d/set-text! (d/by-id "results") (estimate (d/value (d/by-id "velocity")) (d/value (d/by-id "stories"))))))
+  (fn [] (d/set-text! (d/by-id "results") (clj->js (estimate (d/value (d/by-id "velocity")) (d/value (d/by-id "stories")))))))
 
